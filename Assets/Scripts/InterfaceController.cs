d6ddd500c1284e56abe328d45378f10dc1215d17
Andy Hoffman <ahoffman@grovergaming.com>
2021-08-06 11:55:55 -0400

    Figuring out interface layout and components

--- Assets/Scripts/InterfaceController.cs
+++ Assets/Scripts/InterfaceController.cs
@@ -1,8 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InterfaceController : MonoBehaviour
 {
-	public GameObject DefaultLayoutPrefab;
+	public GameObject DefaultInterfaceGroupPrefab;
+    public Button NewPanelButton;
+
+    public Transform Body;
+
+    private List<InterfacePanel> ActivePanels = new List<InterfacePanel>();
+
+    void Start()
+	{
+        NewPanelButton.onClick.AddListener(CreateNewPanel);
+        CreateNewPanel();
+    }
+
+    public void CreateNewPanel()
+	{
+        GameObject newPanel = Instantiate(DefaultInterfaceGroupPrefab, Body);
+        InterfacePanel panel = newPanel.GetComponent<InterfacePanel>();
+        panel.PanelCloseButtonClicked += OnPanelCloseButtonClicked;
+        ActivePanels.Add(panel);
+        Debug.Log("New Panel created");
+    }
+
+    private void OnPanelCloseButtonClicked(InterfacePanel sender)
+    {
+        ActivePanels.Remove(sender);
+        UnityEngine.Object.Destroy(sender.gameObject);
+    }
 }
--- Assets/Scripts/InterfacePanel.cs
+++ Assets/Scripts/InterfacePanel.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InterfacePanel : MonoBehaviour
 {
-	public UnityEngine.UI.Text PanelName;
+	public Text PanelName;
+    public Button CloseButton;
+    public Transform Body;
 
-	void Start()
+    public delegate void PanelEvent(InterfacePanel sender);
+    public PanelEvent PanelCloseButtonClicked;
+
+    void Start()
 	{
-
-	}
+        CloseButton.onClick.AddListener(ClosePanel);
+    }
 
 	public void Initialize(string name)
 	{
 		PanelName.text = name;
 	}
+
+	public void ClosePanel()
+	{
+        PanelCloseButtonClicked(this);
+    }
 }
