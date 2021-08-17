f57187c9ea5629c6e39b1b96ad6303c952af4da9
Andy Hoffman <ahoffman@grovergaming.com>
2021-08-17 12:07:00 -0400

    chat WIP stuff

--- Assets/Scripts/Chat/ChatController.cs
+++ Assets/Scripts/Chat/ChatController.cs
@@ -2,16 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GameComms;
+using RestClient.Core;
+using Newtonsoft.Json;
 
-public static class ChatController
+public class ChatController : MonoBehaviour
 {
-	public static void SendChatPostRequest(ChatMessage message)
+	private static ChatController _instance;
+	public static ChatController Instance
+	{
+		get
+		{
+			if(_instance != null)
+			{
+				return _instance;
+			}
+
+			_instance = GameController.Instance.GetComponent<ChatController>();
+
+			if(_instance == null)
+			{
+				_instance = GameController.Instance.gameObject.AddComponent<ChatController>();
+			}
+
+			return _instance;
+		}
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.T))
+		{
+			SendChatReadRequest(null);
+		}
+	}
+
+	public void SendChatPostRequest(ChatMessage message)
 	{
 
 	}
 
-	public static void SendChatReadRequest(ChatRoom room)
+	public void SendChatReadRequest(ChatRoom room)
 	{
+		ChatMessageReadRequest request = new ChatMessageReadRequest();
+		request.ChatRoomId = 1;
+		request.SenderUserId = 1;
+		request.LastMessageRead = System.DateTime.Now;
 
+		string appendedURL = $"{GameController.Instance.ServerURL}chat/{request.ChatRoomId}";
+		StartCoroutine(RestWebClient.Instance.HttpGet(appendedURL, (r) =>
+		{
+			Debug.LogWarning(r.Data);
+			//ChatMessageResponse response = JsonConvert.DeserializeObject<ChatMessageResponse>(r.Data);
+			//Debug.LogWarning(response);
+		}));
 	}
 }
